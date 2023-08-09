beb1b8340f94f612942e4380c7461123c8af6931
Your Name <you@example.com>
2023-08-09 22:20:56 +0300

    cats ver2

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,25 @@ let beforeImg = document.querySelector('.example__slider-demo--before')
 let afterImg = document.querySelector('.example__slider-demo--after')
 let beforeButton = document.querySelector('.example__control-button--before')
 let afterButton = document.querySelector('.example__control-button--after')
+let menuButton = document.querySelector('.header__mobile-button')
+let navigationList = document.querySelector('.header__navigation-list')
 
-beforeButton.addEventListener("click",function(){
+
+
+menuButton?.addEventListener('click', () => {
+        console.log('ready')
+        navigationList.classList.toggle('header__navigation-list--active')
+        menuButton.classList.toggle('header__mobile-button--active')
+    })
+
+beforeButton?.addEventListener("click", function () {
     beforeImg.style.transition = '1s'
     afterImg.style.transition = '1s'
     beforeImg.style.width = '100%';
     afterImg.style.width = '0%';
     range.value = 0;
 })
-afterButton.addEventListener("click",function(){
+afterButton?.addEventListener("click", function () {
     beforeImg.style.transition = '1s'
     afterImg.style.transition = '1s'
     beforeImg.style.width = '0';
@@ -19,9 +29,10 @@ afterButton.addEventListener("click",function(){
 
     range.value = 100;
 })
-range.addEventListener('input',function(){
+range?.addEventListener('input', function () {
     beforeImg.style.transition = '0.5s'
     afterImg.style.transition = '0.5s'
     beforeImg.style.width = 100 - range.value + '%'
     afterImg.style.width = range.value + '%'
 })
+
